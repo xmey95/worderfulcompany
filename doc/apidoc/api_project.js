5da190f9a6ca9ef8a892089c07c2bf237c72dd08
xmey95 <xmey95@gmail.com>
2018-05-31 18:37:05 +0200

    Started API Documentation creation

---
+++ doc/apidoc/api_project.js
@@ -0,0 +1,19 @@
+define({
+  "title": "WonderfulCompany-API",
+  "url": "https://github.com/xmey95/worderfulcompany",
+  "sampleUrl": "https://github.com/xmey95/worderfulcompany",
+  "description": "",
+  "order": [
+    "General"
+  ],
+  "name": "wonderfulcompany-api",
+  "version": "1.0.0",
+  "defaultVersion": "0.0.0",
+  "apidoc": "0.3.0",
+  "generator": {
+    "name": "apidoc",
+    "time": "2018-05-31T16:34:44.591Z",
+    "url": "http://apidocjs.com",
+    "version": "0.17.6"
+  }
+});
