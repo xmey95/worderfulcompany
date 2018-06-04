bd9d8c3a499eed963d535730c465c39f5bf22076
xmey95 <xmey95@gmail.com>
2018-06-04 18:09:39 +0200

    Created routes for rooms subsection

--- api/routes/router-rooms.js
+++ api/routes/router-rooms.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var config = require('../config.js');
+var auth = require('../auth.js');
 var path = require('path');
 var mysql = require('mysql');
 var router = express.Router(); // new instance of express router
@@ -30,5 +31,45 @@ router.get('/', function(req, res){
         message: 'Benvenuto alle API della Web App "WonderfulCompany"(sezione aulario), effettua una richiesta valida :)'
     });
 });
+
+//Create a room or get all rooms(only superuser)
+router.route('/rooms').post(auth.isAuthenticated, function(req, res){
+
+}).get(auth.isAuthenticated, function(req, res){
+
+});
+
+//Get, modify or delete a room or get all rooms(only superuser)
+router.route('/rooms/:room').get(auth.isAuthenticated, function(req, res){
+
+}).put(auth.isAuthenticated, function(req, res){
+
+}).delete(auth.isAuthenticated, function(req, res){
+
+});
+
+//find rooms by filters passed by post parameters
+router.route('/findrooms').post(auth.isAuthenticated, function(req, res){
+
+});
+
+//find bookings of requesting user
+router.route('/bookings').get(auth.isAuthenticated, function(req, res){
+
+});
+
+//find bookings for the specified room and submit a list of interval to booking
+router.route('/bookings/:room').get(auth.isAuthenticated, function(req, res){
+
+}).post(auth.isAuthenticated, function(req, res){
+
+});
+
+//Delete specified booking(is requesting user is owner of the booking)
+router.route('/bookings/:booking').delete(auth.isAuthenticated, function(req, res){
+
+});
+
+
 
 module.exports = router;
--- api/setup_db.js
+++ api/setup_db.js
@@ -11,7 +11,7 @@ var setup_db = () => {
 
 console.log("Starting DB Setup...")
 
-var users = 'CREATE TABLE if not exists users (id integer primary key AUTO_INCREMENT, password text not null, name text not null, surname text not null, email text not null)'
+var users = 'CREATE TABLE if not exists users (id integer primary key AUTO_INCREMENT, password text not null, name text not null, surname text not null, email text not null, superuser boolean not null default false)'
 
 var absence = 'CREATE TABLE if not exists absences (id integer primary key AUTO_INCREMENT, id_user integer not null, state integer not null, reason text not null, justified boolean, start_date date, end_date date, foreign key(id_user) references users(id))'
 
