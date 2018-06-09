b5c8316d0fab1c0084908d74e4dd8688c2c2b7aa
ValentinaCostanza <valentina.costanza.11@gmail.com>
2018-06-09 20:34:37 +0200

    added route to insert and delete room's accessories and added route for editing the room

--- api/routes/router-rooms.js
+++ api/routes/router-rooms.js
@@ -117,10 +117,68 @@ router.route('/rooms/:version').get(auth.isAuthenticated, function(req, res){
 
 });
 
+//add accessories databases 
+router.route('/accessories').post(auth.isAuthenticated, function(req, res){
 
+    pool.getConnection(function(err, connection) {
+        // Use the connection
+        var post  = {name: req.body.name, description: req.body.description};
+        connection.query('INSERT INTO ?? SET ?', ['accessories', post], function (err, results, fields) {
+          // And done with the connection.
+          connection.release();
+  
+          if (err) return res.status(500).send(JSON.stringify({success:false, error:err}))
+          else res.status(201).send(JSON.stringify({success:true, error:null}));
+        });
+      });
 
-//Get, modify or delete a room or get all rooms(only superuser)
+});
+
+//_______________ manca la modifica degli accessori della stanza ...
+//Modify or delete a room or get all rooms(only superuser)
 router.route('/rooms/:room').put(auth.isAuthenticated, function(req, res){
+
+    pool.getConnection(function(err, connection) {
+
+        var id_room = req.params.room;
+
+        connection.query('SELECT * FROM ?? WHERE ?? = ?', ['rooms', 'id', id_room], function (err, results, fields) {
+            
+            if (err) {
+              connection.release();
+              return res.status(500).send(JSON.stringify({success:false, error:err}));
+            }
+            if (!results || results.length == 0) {
+              connection.release();
+              return res.status(404).send(JSON.stringify({success:false, error:"ROOM_NOT_FOUND"}));
+            }
+
+            var name = results[0].name;
+            var floor = results[0].floor;
+            var capacity = results[0].capacity;
+
+            if(req.body.name){
+                name = req.body.name;
+            }
+
+            if(req.body.floor){
+                floor = req.body.floor;
+            }
+
+            if(req.body.capacity){
+                capacity = req.body.capacity;
+            }
+
+            connection.query('UPDATE ?? SET ?? = ?, ?? = ?, ?? = ? WHERE ?? = ?', ['rooms', 'name', name, 'floor', floor, 'capacity', capacity, 'id', id_room], function (err, results, fields) {
+                connection.release();
+                if (err) return res.status(500).send(JSON.stringify({success:false, error:err}));
+                if (!results) return res.status(404).send(JSON.stringify({success:false, error:"ROOM_NOT_FOUND"}));
+                res.status(201).send(JSON.stringify({success:true, error:null}));
+                
+            });//update 'rooms'
+            
+        });//select 'rooms'
+    });
 
 }).delete(auth.isAuthenticated, function(req, res){
     
@@ -143,7 +201,121 @@ router.route('/rooms/:room').put(auth.isAuthenticated, function(req, res){
 });
 
 router.route('/rooms/:room/:version').get(auth.isAuthenticated, function(req, res){
+    pool.getConnection(function(err, connection) {
 
+        var id_room = req.params.room;
+
+        connection.query('SELECT * FROM ?? WHERE ?? = ?', ['rooms', 'id', id_room], function (err, results, fields) {
+            
+            if (err) {
+              connection.release();
+              return res.status(500).send(JSON.stringify({success:false, error:err}));
+            }
+            if (!results || results.length == 0) {
+              connection.release();
+              return res.status(404).send(JSON.stringify({success:false, error:"ROOM_NOT_FOUND"}));
+            }
+
+            var room = results;
+
+            connection.query('SELECT * FROM ?? WHERE ?? = ?', ['accessoriesrooms', 'id_room', id_room], function (err, results, fields){
+
+                connection.release();
+                if (err) return res.status(500).send(JSON.stringify({success:false, error: err}));
+                if (!results) return res.status(404).send(JSON.stringify({success:false, error:"ROOMS_NOT_FOUND"}));
+
+                room.accessories = [];
+                for(var j=0; j<results.length; j++){
+                    var object = {
+                        "accessory": results[j].id_accessory,
+                        "quantity": results[j].quantity,
+                    }
+                    room.accessories.push(object);   
+                }
+                
+                if(req.params.version=='false'){
+                        return res.status(200).send(JSON.stringify({success:true, error:null, room: room, accessories: room.accessories}));
+                }else{
+                        var string = JSON.stringify(rooms);
+                        string = crypto.createHash('sha1').update(string).digest('hex');
+                        return res.status(200).send(JSON.stringify({success:true, error:null, version: string}));
+                }   
+            });
+
+
+        });
+    });
+
+});
+
+//aggiungi accessori in una stanza 
+router.route('/rooms/:room/accessories').post(auth.isAuthenticated, function(req, res){
+    
+    pool.getConnection(function(err, connection) {
+        // Use the connection
+        var id_room = req.params.room;
+
+        connection.query('SELECT * FROM ?? WHERE ?? = ?', ['rooms', 'id', id_room], function (err, results, fields) {
+            
+            if (err) {
+              connection.release();
+              return res.status(500).send(JSON.stringify({success:false, error:err}));
+            }
+            if (!results || results.length == 0) {
+              connection.release();
+              return res.status(404).send(JSON.stringify({success:false, error:"ROOM_NOT_FOUND"}));
+            }
+
+            var post  = {id_room: id_room, id_accessory: req.body.id_accessory, quantity: req.body.quantity};
+            connection.query('INSERT INTO ?? SET ?', ['accessoriesrooms', post], function (err, results, fields) {
+            // And done with the connection.
+            connection.release();
+    
+            if (err) return res.status(500).send(JSON.stringify({success:false, error:err}))
+            else res.status(201).send(JSON.stringify({success:true, error:null}));
+            }); 
+        });
+    });
+});
+
+//rimuovi accessori in una stanza
+router.route('/rooms/:room/accessories/:accessory').delete(auth.isAuthenticated, function(req, res){
+    pool.getConnection(function(err, connection) {
+        // Use the connection
+        var id_room = req.params.room;
+        var id_accessory = req.params.accessory;
+
+        connection.query('SELECT * FROM ?? WHERE ?? = ?', ['rooms', 'id', id_room], function (err, results, fields) {
+            
+            if (err) {
+              connection.release();
+              return res.status(500).send(JSON.stringify({success:false, error:err}));
+            }
+            if (!results || results.length == 0) {
+              connection.release();
+              return res.status(404).send(JSON.stringify({success:false, error:"ROOM_NOT_FOUND"}));
+            }
+
+            connection.query('SELECT * FROM ?? WHERE ?? = ?', ['accessories', 'id', id_accessory], function (err, results, fields) {
+                if (err) {
+                    connection.release();
+                    return res.status(500).send(JSON.stringify({success:false, error:err}));
+                }
+                if (!results || results.length == 0) {
+                connection.release();
+                return res.status(404).send(JSON.stringify({success:false, error:"ACCESSORY_NOT_FOUND"}));
+                }
+
+                connection.query('DELETE FROM ?? WHERE ?? = ? AND ?? = ?', ['accessoriesrooms', 'id_room', id_room, 'id_accessory', id_accessory], function (err, results, fields) {
+                    // And done with the connection.
+                    connection.release();
+            
+                    if (err) return res.status(500).send(JSON.stringify({success:false, error:err}))
+                    else res.status(201).send(JSON.stringify({success:true, error:null}));
+                });
+            });
+        });
+    });
 });
 
 //find rooms by filters passed by post parameters
